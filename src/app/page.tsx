b5a76c2edd73e83fd47ee84a86d72c9d5c382853
mack1ch <dimastepanov.1995@mail.ru>
2024-06-14 16:19:50 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,6 @@ import { useCookies } from "react-cookie";
 import { uid } from "uid";
 
 export default function Home() {
-  const [cookies, setCookie] = useCookies(["user-id"]);
-  const isDevMode = process.env.NODE_ENV !== "production";
-  useEffect(() => {
-    if (cookies["user-id"]) {
-    } else {
-      setCookie("user-id", uid(16), {
-        secure: true,
-      });
-    }
-  }, []);
   return (
     <>
       <AppLayout>
