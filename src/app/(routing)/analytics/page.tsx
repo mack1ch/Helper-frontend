5011ba138c90298989b21f47f14d418530403b1d
mack1ch <dimastepanov.1995@mail.ru>
2024-06-14 15:28:05 +0500

    added: cookie

--- src/app/(routing)/analytics/page.tsx
+++ src/app/(routing)/analytics/page.tsx
@@ -2,6 +2,7 @@ import { AuthForm } from "@/features/authForm-slice/auth";
 import { AppLayout } from "@/shared/layouts/appLayout";
 
 export default function Home() {
+  
   return (
     <>
       <AppLayout>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
+"use client";
+
 import { AuthForm } from "@/features/authForm-slice/auth";
 import { AppLayout } from "@/shared/layouts/appLayout";
 import { Helper } from "@/widgets/helper-slice/helper";
+import { useEffect } from "react";
+import { useCookies } from "react-cookie";
+import { uid } from "uid";
 
 export default function Home() {
+  const [cookies, setCookie] = useCookies(["user-id"]);
+  useEffect(() => {
+    if (cookies["user-id"]) {
+    } else {
+      setCookie("user-id", uid(16));
+    }
+  }, []);
   return (
     <>
       <AppLayout>
         <AuthForm />
-        <Helper/>
+        <Helper />
       </AppLayout>
     </>
   );
