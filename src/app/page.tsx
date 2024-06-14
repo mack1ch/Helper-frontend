cd517b305271da45a886671cc105fa5d496f4cf1
mack1ch <dimastepanov.1995@mail.ru>
2024-06-14 16:11:55 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
     if (cookies["user-id"]) {
     } else {
       setCookie("user-id", uid(16), {
-        sameSite: "none",
+        secure: true,
       });
     }
   }, []);
